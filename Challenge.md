d19369fab26ae8bc87b2dcf8a1f47bfc4fc7b363
Henrique Alexandre <Henriquealexandredec@gmail.com>
2023-06-26 22:34:16 -0300

    arrays-test done

--- Challenge.md
+++ Challenge.md
@@ -1,3 +1,33 @@
-Utilização:
-Abra o Index.HTML
-Nele terá 
+# Guia de Utilização - Array Test
+
+O Array Test é uma página web interativa que permite realizar testes lógicos simples, como verificar o número mais alto de uma array, encontrar a maior nome e a soma de um array de números. Este guia fornecerá instruções sobre como usar a página e realizar cada teste.
+
+## Pré-requisitos
+
+- Navegador da web atualizado (recomenda-se Google Chrome, Mozilla Firefox, ou Microsoft Edge).
+
+## Passo a Passo
+
+1. Faça o download dos arquivos necessários do Array Test (HTML, JavaScript) e coloque-os em uma pasta no seu computador.
+
+2. Abra o arquivo `index.html` no seu navegador, clicando com o botão direito do mouse no arquivo e selecionando "Abrir com" e, em seguida, escolhendo o navegador da sua preferência.
+
+3. Ao carregar a página, você verá o título "Array Test" e três seções de teste: "Largest Number in array" (Maior número na lista), "Longest Name in Array" (Maior nome na lista) e "Sum of the Array" (Soma da lista).
+
+4. **Teste: Maior número na lista**
+
+   - Na seção "Largest Number in array", insira uma lista de números separados por vírgula no campo de texto. (já tem uma para testes rápidos)
+   - Clique no botão "Find Largest Number" para executar o teste.
+   - O resultado será exibido logo abaixo
+
+5. **Teste: Maior nome na lista**
+  
+   - Na seção "Longest Name in Array", insira uma lista de nomes separados por vírgula no campo de texto. (já tem uma para testes rápidos)
+   - Clique no botão "Find Longest Name" para executar o teste.
+   - O resultado será exibido logo abaixo
+
+6. **Teste: Soma da lista**
+  
+   - Na seção "Sum of the Array", insira uma lista de números separados por vírgula no campo de texto. (já tem uma para testes rápidos)
+   - Clique no botão "Find Sum of Array" para executar o teste.
+   - O resultado será exibido logo abaixo
--- script.js
+++ script.js
@@ -1,47 +1,31 @@
-// Função que retorna se o valor é primo ou não
-function isPrime() {
-  Number = document.getElementById("prime-number").value;
-  Result = document.getElementById("prime-result");
-  // Desconsiderando 0 e números negativos
-  if (Number == 1) {
-    Result.innerHTML = "1 is not a prime number";
-  }
-  else {
-    for (let i = 2; i < Number; i++) {
-      //Se o resto da divisão do número 2 ao número digitado for 0, ele não é primo
-      if (Number % i == 0) {
-        Result.innerHTML = Number + " is not a prime number";
-        break;
-      }
-      else {
-        Result.innerHTML = Number + " is a prime number";
-      }
-    }
-  }
+function findLargestNumber(){
+  // Adquire a Array inteira
+  numbersArray = document.getElementById("numbers-array").value;
+  // Remove os colchetes e transforma em um Array
+  numbersArray = numbersArray.replace(/[\[\]]/g , "").split(",");
+  // Transforma os valores em números (Se tiver letras, vai dar erro)
+  numbers = numbersArray.map(Number);
+  // Encontra o maior número
+  largestNumber = Math.max.apply(Math, numbers);
+  // Mostra o maior número
+  document.getElementById("largestNumber").innerHTML ="Largest Number in Array: " + largestNumber;
 }
 
-// Checa se a palavra ao contrário fica igual
-function isPalindrome() {
-  Word = document.getElementById("palindrome-word").value;
-  Result = document.getElementById("palindrome-result");
-  // Funciona com letras acentuadas, maiúsculas e minúsculas
-  if (Word.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "") == Word.split("").reverse().join("").toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")) {
-    Result.innerHTML = Word + " is a palindrome";
-  }
-  else {
-    Result.innerHTML = Word + " is not a palindrome";
-  }
+function findLongestName(){
+  namesArray = document.getElementById("names-array").value;
+  namesArray = namesArray.replace(/[\[\]]/g , "").split(",");
+  // Encontra o maior nome (Reduce interage com cada elemento do Array, substituindo loops )
+  longestName = namesArray.reduce(function(a, b) { return a.length > b.length ? a : b; });
+  // Mostra o maior nome
+  document.getElementById("longestName").innerHTML ="Longest Name in Array: " + longestName;
 }
 
-// Checa se o ano é bissexto
-function isLeapYear() {
-  Year = document.getElementById("leap-year").value;
-  Result = document.getElementById("leap-year-result");
-  // Se o ano for divisível por 4 e não for divisível por 100 ou se for divisível por 400, ele é bissexto
-  if (Year % 4 == 0 && Year % 100 != 0 || Year % 400 == 0) {
-    Result.innerHTML = Year + " is a leap year";
-  }
-  else {
-    Result.innerHTML = Year + " is not a leap year";
-  }
+function calculateSum(){
+  numbersArray = document.getElementById("numbers-array-sum").value;
+  numbersArray = numbersArray.replace(/[\[\]]/g , "").split(",");
+  numbers = numbersArray.map(Number);
+  // Soma todos os valores do Array (Reduce tira a necessidade de loop)
+  sum = numbers.reduce(function(a, b) { return a + b; });
+  // Mostra a soma
+  document.getElementById("sum").innerHTML ="Sum of Array: " + sum;
 }
